01945b8a17c2585b2c78746a35f3b220e0795268
Fahri Rivaldy <fahrirvaldy69@gmail.com>
2026-05-26 13:03:08 +0700

    change database and repo

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,21 @@
 // Import fungsi-fungsi inisialisasi dari SDK Firebase
 import { initializeApp } from "firebase/app";
 import { initializeFirestore, enableIndexedDbPersistence } from "firebase/firestore"; // Modul Database (Firestore)
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAndNyS0_mDikrAaoTMQByWzL_zpzWVB7g",
+  authDomain: "l10-meetings-gwk.firebaseapp.com",
+  projectId: "l10-meetings-gwk",
+  storageBucket: "l10-meetings-gwk.firebasestorage.app",
+  messagingSenderId: "213426293126",
+  appId: "1:213426293126:web:a87d1b65ebf50d449d0407",
+  measurementId: "G-D98CKK9GX0"
 };
 
 // 1. Inisialisasi Aplikasi Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // 2. Inisialisasi Layanan Database
 export const db = initializeFirestore(app, {
